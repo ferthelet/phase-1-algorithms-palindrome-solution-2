eba56f533da8c07a24f9d052cbf65a2d0f7fd690
Juan Salazar <ferthelet@gmail.com>
2023-06-09 12:43:52 -0500

    adds required code and pseudo code

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedWord = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversedWord += word[i];
+  }
+  return reversedWord === word;
 }
 
 /* 
   Add your pseudocode here
+
+  - create a variable to hold the reversed word
+  - loop through the word backwards
+  - add each letter to the reversed word
+  - return whether the reversed word is equal to the original word
+
 */
 
 /*
   Add written explanation of your solution here
+
+  I looped through the word backwards and added each letter to a new string. Then I returned whether the reversed word was equal to the original word.
+  
 */
 
 // You can run `node index.js` to view these console logs
